780b00bc5962ba2779383b2ea336bbf22f94c6dd
Tira <atiraodhner@gmail.com>
2015-01-11 11:17:45 -0800

    added grunt to rebuild css when less files change -used grunt-exec instead of grunt-contrib-less since it works on windows.

---
+++ Gruntfile.js
@@ -0,0 +1,19 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+
+    watch: {
+      less: {
+        files: ['src/**/*.less'],
+        tasks: 'exec:compile_less'
+      }
+    },
+    exec: {
+      compile_less:'lessc ./src/less/ordial.less ./src/css/ordial.css'
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-exec');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.registerTask('default', ['watch']);
+}
