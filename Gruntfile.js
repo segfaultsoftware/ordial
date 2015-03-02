11e3984386932ae98fd4c2263b05c7ac2fa7c14f
Tira Odhner <atiraodhner@gmail.com>
2015-03-01 17:13:42 -0800

    We no longer need to manually include files.
    Grunt default task now starts a webserver and watches to include new .js files
    [finishes #89351192]

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,14 +6,43 @@ module.exports = function(grunt) {
       less: {
         files: ['src/**/*.less'],
         tasks: 'exec:compile_less'
+      },
+      includeNewSources: {
+        files: ['src/**/*.js'],
+        tasks: 'includeSource'
+      },
+      includeNewSpecs: {
+        files: ['spec/**/*.js'],
+        tasks: 'includeSource:spec'
       }
     },
     exec: {
       compile_less: 'lessc ./src/less/ordial.less ./src/css/ordial.css'
+    },
+    connect: {
+      server: {
+        options: {
+          port: 8000
+        }
+      }
+    },
+    includeSource: {
+      app: {
+        files: {
+          'index.html': 'src/html/index.template.html'
+        }
+      },
+      spec: {
+        files: {
+          'SpecRunner.html': 'src/html/SpecRunner.template.html'
+        }
+      }
     }
   });
 
   grunt.loadNpmTasks('grunt-exec');
   grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.registerTask('default', ['watch']);
+  grunt.loadNpmTasks('grunt-include-source');
+  grunt.loadNpmTasks('grunt-contrib-connect');
+  grunt.registerTask('default', ['connect:server', 'watch']);
 };
