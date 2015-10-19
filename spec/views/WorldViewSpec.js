ff88b3999ff0d83583d818f98cf61f9e5c8319c4
Dan Junger and Tira Odhner <djunger+atiraodhner@gmail.com>
2015-10-19 16:18:42 -0700

    Remove unnecessary html rendering of the world, now that we have pixi!
    - and fix failing specs.
    
    [#105813626]

--- spec/views/WorldViewSpec.js
+++ spec/views/WorldViewSpec.js
@@ -8,6 +8,27 @@ describe("WorldView", function() {
     it('creates a renderer', function() {
       expect(worldView.renderer).toBeDefined();
     });
+
+    describe('the grid', function() {
+      beforeEach(function() {
+        world.height = Math.ceil(window.singletonContext.randomNumberGenerator.random() * 100);
+        world.width = Math.ceil(window.singletonContext.randomNumberGenerator.random() * 100);
+        worldView = new WorldView({model: world, el: '#world'});
+        worldView.render();
+      });
+
+      it("should have height matching the world's height", function() {
+        var element = $("canvas")[0];
+        var rect = element.getBoundingClientRect();
+        expect(rect.height).toEqual(world.height * worldView.cellSize);
+      });
+
+      it("should have width matching the world's width", function() {
+        var element = $("canvas")[0];
+        var rect = element.getBoundingClientRect();
+        expect(rect.width).toEqual(world.width * worldView.cellSize);
+      });
+    });
   });
 
   describe("#imageForModel", function() {
@@ -69,24 +90,6 @@ describe("WorldView", function() {
     });
   });
 
-  describe('the grid', function() {
-    beforeEach(function() {
-      world.height = Math.ceil(window.singletonContext.randomNumberGenerator.random() * 100);
-      world.width = Math.ceil(window.singletonContext.randomNumberGenerator.random() * 100);
-      worldView.render();
-    });
-
-    it("should have rows matching the world's height", function() {
-      expect(worldView.$('tr').length).toEqual(world.height);
-    });
-
-    it("should have columns matching the world's width", function() {
-      _.each(worldView.$('tr'), function(tr) {
-        expect($(tr).find('td').length).toEqual(world.width);
-      });
-    });
-  });
-
   describe("in a world with Rob", function() {
     var rob, robLocation;
     beforeEach(function() {
@@ -96,19 +99,6 @@ describe("WorldView", function() {
       robLocation = {x: 0 * worldView.cellSize, y: 0 * worldView.cellSize};
 
       worldView.render();
-    });
-
-    it("should draw Rob at the right place", function() {
-      var robsCell = worldView.$el.find("table tr:nth-child(5) td:nth-child(5)");
-      expect(robsCell.find('.critter').length).toEqual(1);
-    });
-
-    it('should draw nothing everywhere else', function() {
-      expect(worldView.$el.find('.critter').length).toEqual(1);
-    });
-
-    it('should draw Rob facing the north', function() {
-      expect(worldView.$el.find('.critter').hasClass('north')).toBeTruthy();
     });
 
     describe("clicking on the critter", function() {
@@ -136,34 +126,6 @@ describe("WorldView", function() {
 
         expect(callbackSpy).toHaveBeenCalledWith(rob);
       });
-    });
-  });
-
-  describe("in a world with resources", function() {
-    beforeEach(function() {
-      var resource = new Resource();
-      world.place(resource, {x: 3, y: 3});
-
-      worldView.render();
-    });
-
-    it("should draw the resource at the right place", function() {
-      var resourceCell = worldView.$el.find("table tr:nth-child(4) td:nth-child(4)");
-      expect(resourceCell.find(".resource").length).toEqual(1);
-    });
-  });
-
-  describe("in a world with rocks", function() {
-    beforeEach(function() {
-      var rock = new Rock();
-      world.place(rock, {x: 4, y: 4});
-
-      worldView.render();
-    });
-
-    it("should draw the rock at the right place", function() {
-      var rockCell = worldView.$el.find("table tr:nth-child(5) td:nth-child(5)");
-      expect(rockCell.find(".rock").length).toEqual(1);
     });
   });
 });
--- src/javascript/views/WorldView.js
+++ src/javascript/views/WorldView.js
@@ -1,6 +1,6 @@
 $(function() {
   WorldView = Backbone.View.extend({
-    initialize: function(){
+    initialize: function() {
       this.cellSize = 15;
       this.renderer = PIXI.autoDetectRenderer(
         this.model.width * this.cellSize,
@@ -11,20 +11,14 @@ $(function() {
     render: function() {
       var stage = new PIXI.Container();
 
-      // render the grid
-      // render the world inhabitants
-      var tableEl = document.createElement('table');
       this.$el.html('');
-      for(var row = 0; row < this.model.height; row++) {
-        var rowEl = document.createElement('tr');
-        for(var col = 0; col < this.model.width; col++){
-          var thing = this.model.getThingAt({x: col, y: row});
-          var thingView = this.renderThingAt(thing);
-          var tdEl = document.createElement('td');
+      for(var gridY = 0; gridY < this.model.height; gridY++) {
+        for(var gridX = 0; gridX < this.model.width; gridX++) {
+          var thing = this.model.getThingAt({x: gridX, y: gridY});
           var texture = PIXI.utils.TextureCache[this.imageForModel(thing)];
           var sprite = new PIXI.Sprite(texture);
-          sprite.position.x = col*this.cellSize;
-          sprite.position.y = row*this.cellSize;
+          sprite.position.x = gridX * this.cellSize;
+          sprite.position.y = gridY * this.cellSize;
           sprite.interactive = true;
           sprite.on('mousedown', (function(thing) {
             return function() {
@@ -33,44 +27,23 @@ $(function() {
           })(thing));
           stage.addChild(sprite);
 
-          if(thingView){
-            tdEl.appendChild(thingView);
-          }
-          rowEl.appendChild(tdEl);
         }
-        tableEl.appendChild(rowEl);
       }
       this.renderer.render(stage);
       this.el.appendChild(this.renderer.view);
       return this;
     },
 
-    imageForModel: function(thing){
-      if(thing instanceof Critter){
+    imageForModel: function(thing) {
+      if(thing instanceof Critter) {
         return "ordial_critter_" + thing.direction.toLowerCase() + "_" + thing.color + ".png";
       }
-      else if (thing instanceof Resource) {
+      else if(thing instanceof Resource) {
         return 'resource.png';
       }
-      else if (thing instanceof Rock) {
+      else if(thing instanceof Rock) {
         return 'rock.png';
       }
-    },
-    renderThingAt: function(thing){
-      var view;
-
-      if(thing){
-        if(thing instanceof Critter){
-          view = new CritterView({model: thing});
-        }
-        else if (thing instanceof Resource) {
-          view = new ResourceView();
-        }
-        else if (thing instanceof Rock) {
-          view = new RockView();
-        }
-      }
-      return view ? view.render().el: null;
     }
   });
 });
