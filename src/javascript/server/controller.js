bbee1e688ab624596601ad5de656fde293f4053c
Tira Odhner <atiraodhner@gmail.com>
2019-09-16 13:22:39 -0400

    Disable the headless server to save my battery.

--- src/javascript/server/controller.js
+++ src/javascript/server/controller.js
@@ -22,7 +22,6 @@ app.post('/world', function (request, response) {
 app.use(express.static('public'));
 app.use('/vendor', express.static('vendor'));
 
-app.use('/assets', express.static('public/assets'));
 app.use('/compiled/css', express.static('public/compiled/css'));
 app.use('/src/javascript/browser', express.static('src/javascript/browser'));
 app.use('/src/javascript/lib', express.static('src/javascript/lib'));
