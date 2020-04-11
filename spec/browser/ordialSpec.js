caeff14ec1176df9bf98e04180dbe23e030e9af5
Tira Odhner <atiraodhner@gmail.com>
2020-04-10 22:38:35 -0400

    Make sure ordial gets an $el in tests

--- spec/browser/ordialSpec.js
+++ spec/browser/ordialSpec.js
@@ -12,7 +12,7 @@ describe("Ordial", function () {
 
     loaderPromise = jasmine.createSpyObj('loaderPromise', ["load"]);
     spyOn(PIXI.loader, "add").and.returnValue(loaderPromise);
-    ordial = new Ordial();
+    ordial = new Ordial({ el: $('#ordial') });
   });
 
   describe("loading images", function () {
@@ -30,7 +30,7 @@ describe("Ordial", function () {
       });
 
       it("renders the worldView", function () {
-        ordial = new Ordial();
+        ordial = new Ordial({ el: $('#ordial') });
         WorldView.prototype.render.calls.reset();
         loadCallback();
         expect(WorldView.prototype.render).toHaveBeenCalled();
@@ -40,7 +40,7 @@ describe("Ordial", function () {
 
   it("should have a pause button", function () {
     expect(ordial.$el.find("#pause-button")).toBeTruthy();
-    expect($(ordial.$el.find('#pause-button').selector).text()).toEqual('resume');
+    expect(ordial.$el.find('#pause-button').text()).toEqual('resume');
   });
 
   it("should default to paused", function () {
@@ -54,39 +54,39 @@ describe("Ordial", function () {
   describe('seed input field', function () {
     it('should start with a random seed', function () {
       var firstVal = ordial.$el.find('#seed-input').val();
-      ordial = new Ordial({ scheduler: ordialScheduler });
+      ordial = new Ordial({ scheduler: ordialScheduler, el: $('#ordial') });
       ordial.render();
-      expect($(ordial.$el.find('#seed-input').selector).val()).not.toEqual(firstVal);
+      expect(ordial.$el.find('#seed-input').val()).not.toEqual(firstVal);
     });
 
     it('should not just use the next call to Math.random as the seed', function () {
-      $(ordial.$el.find('#seed-input').selector).val('abc').blur();
-      $(ordial.$el.find('#pause-button').selector).click();
+      ordial.$el.find('#seed-input').val('abc').blur();
+      ordial.$el.find('#pause-button').click();
 
-      ordial = new Ordial({ scheduler: ordialScheduler });
+      ordial = new Ordial({ scheduler: ordialScheduler, el: $('#ordial') });
       ordial.render();
-      $(ordial.$el.find('#pause-button').selector).click();
+      ordial.$el.find('#pause-button').click();
       var undefinedRandomVal = singletonContext.randomNumberGenerator.random(1000000);
 
-      ordial = new Ordial({ scheduler: ordialScheduler });
+      ordial = new Ordial({ scheduler: ordialScheduler, el: $('#ordial') });
       ordial.render();
-      $(ordial.$el.find('#seed-input').selector).val('abc').blur();
-      $(ordial.$el.find('#pause-button').selector).click();
+      ordial.$el.find('#seed-input').val('abc').blur();
+      ordial.$el.find('#pause-button').click();
 
-      ordial = new Ordial({ scheduler: ordialScheduler });
+      ordial = new Ordial({ scheduler: ordialScheduler, el: $('#ordial') });
       ordial.render();
-      $(ordial.$el.find('#pause-button').selector).click();
+      ordial.$el.find('#pause-button').click();
       expect(singletonContext.randomNumberGenerator.random(1000000)).not.toEqual(undefinedRandomVal);
     });
 
     describe('with different seeds after starting the world', function () {
       var firstVal;
       beforeEach(function () {
-        $(ordial.$el.find('#pause-button').selector).click();
+        ordial.$el.find('#pause-button').click();
         firstVal = singletonContext.randomNumberGenerator.random(1000000);
-        ordial = new Ordial({ scheduler: ordialScheduler });
+        ordial = new Ordial({ scheduler: ordialScheduler, el: $('#ordial') });
         ordial.render();
-        $(ordial.$el.find('#pause-button').selector).click();
+        ordial.$el.find('#pause-button').click();
       });
 
       it('should lead to different random numbers', function () {
@@ -97,17 +97,17 @@ describe("Ordial", function () {
     describe('user changing the seed value and after starting the world', function () {
       var firstVal, secondVal;
       beforeEach(function () {
-        $(ordial.$el.find('#seed-input').selector).val('abc')
-        $(ordial.$el.find('#seed-input').selector).blur();
-        $(ordial.$el.find('#pause-button').selector).click();
+        ordial.$el.find('#seed-input').val('abc')
+        ordial.$el.find('#seed-input').blur();
+        ordial.$el.find('#pause-button').click();
 
         firstVal = singletonContext.randomNumberGenerator.random(1000000);
         secondVal = singletonContext.randomNumberGenerator.random(1000000);
-        ordial = new Ordial({ scheduler: ordialScheduler });
+        ordial = new Ordial({ scheduler: ordialScheduler, el: $('#ordial')});
         ordial.render();
-        $(ordial.$el.find('#seed-input').selector).val('abc');
-        $(ordial.$el.find('#seed-input').selector).blur();
-        $(ordial.$el.find('#pause-button').selector).click();
+        ordial.$el.find('#seed-input').val('abc');
+        ordial.$el.find('#seed-input').blur();
+        ordial.$el.find('#pause-button').click();
       });
 
       it('should produce the same random numbers as previously with the same seed', function () {
@@ -116,10 +116,10 @@ describe("Ordial", function () {
       });
 
       it('should produce different random numbers with a different seed', function () {
-        ordial = new Ordial({ scheduler: ordialScheduler });
+        ordial = new Ordial({ scheduler: ordialScheduler, el: $('#ordial')});
         ordial.render();
         ordial.$el.find('#seed-input').val('abcd').blur();
-        $(ordial.$el.find('#pause-button').selector).click();
+        ordial.$el.find('#pause-button').click();
         expect(singletonContext.randomNumberGenerator.random(1000000)).not.toEqual(firstVal);
         expect(singletonContext.randomNumberGenerator.random(1000000)).not.toEqual(secondVal);
       });
@@ -133,21 +133,21 @@ describe("Ordial", function () {
 
     it('disables the seed input', function () {
       spyOn(ordial.seedView, 'disableInput');
-      $(ordial.$el.find('#pause-button').selector).click();
+      ordial.$el.find('#pause-button').click();
       expect(ordial.seedView.disableInput).toHaveBeenCalled();
     });
 
     it("toggles the paused flag", function () {
       expect(ordial.paused).toBeTruthy();
-      $(ordial.$el.find('#pause-button').selector).click();
+      ordial.$el.find('#pause-button').click();
       expect(ordial.paused).toBeFalsy();
-      $(ordial.$el.find('#pause-button').selector).click();
+      ordial.$el.find('#pause-button').click();
       expect(ordial.paused).toBeTruthy();
     });
 
     describe("when unpausing", function () {
       beforeEach(function () {
-        $(ordial.$el.find('#pause-button').selector).click();
+        ordial.$el.find('#pause-button').click();
       });
 
       it("should updateWorld", function () {
@@ -155,14 +155,14 @@ describe("Ordial", function () {
       });
 
       it('should change text to "pause"', function () {
-        expect($(ordial.$el.find('#pause-button').selector).text()).toEqual('pause');
+        expect(ordial.$el.find('#pause-button').text()).toEqual('pause');
       });
     });
 
     describe("when pausing", function () {
       beforeEach(function () {
         ordial.paused = false;
-        $(ordial.$el.find('#pause-button').selector).click();
+        ordial.$el.find('#pause-button').click();
       });
 
       it('should call updateWorld', function () {
@@ -170,7 +170,7 @@ describe("Ordial", function () {
       });
 
       it('should change text to "resume"', function () {
-        expect($(ordial.$el.find('#pause-button').selector).text()).toEqual('resume');
+        expect(ordial.$el.find('#pause-button').text()).toEqual('resume');
       });
     });
   });
@@ -183,7 +183,7 @@ describe("Ordial", function () {
 
       it("should render the world", function () {
         ordial.updateWorld();
-        expect($(ordial.$('#world').selector).length).toBe(1);
+        expect(ordial.$('#world').length).toBe(1);
       });
 
       it("should update the world", function () {
@@ -205,7 +205,7 @@ describe("Ordial", function () {
 
       it("should render the world", function () {
         ordial.updateWorld();
-        expect($(ordial.$('#world').selector).length).toBe(1);
+        expect(ordial.$('#world').length).toBe(1);
       });
 
       it("should not update the world", function () {
